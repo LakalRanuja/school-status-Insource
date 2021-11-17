d09a0b7f9d4cee36c3b170193ea44c4fe65f97d3
lakal <wadduwage.thilak@gmail.com>
2021-11-18 01:10:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import _presentTeachers from './components/presentTeacher';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <_presentTeachers/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/presentTeacher.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './css/presentTeacher.css'
+
+function presentTeacher() {
+    return (
+        <div className="main-teacher">
+            <div className="sub-teacher">
+                <div className="tab1 col-2">
+                    <p className="tab2">Sister Sophia</p>
+                </div>
+                <div className="tab3">
+                    <p className="tab2">Father Fracis</p>
+                </div>
+                <div className="tab4">
+                    <p className="tab2">Sister Maria</p>
+                </div>
+                <div className="tab5">
+                    <p className="tab2">Sister Amonia</p>
+                </div>
+                <div className="tab6">
+                    <p className="tab2">Father Cristoper</p>
+                </div>
+                <div className="tab7">
+                    <p className="tab2">Sister Phenomena</p>
+                </div>
+            </div>
+        </div>
+
+    )
+
+};
+
+export default presentTeacher;
